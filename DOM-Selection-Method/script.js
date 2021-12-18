10e2f78585601b11b25d28d40516e56b2ba1b362
cattyproject <ivanilearningproject@gmail.com>
2021-12-18 22:01:38 +0700

    membuat branch baru dan membuat file manipulaso DOM di dalamnya

---
+++ DOM-Selection-Method/script.js
@@ -0,0 +1,5 @@
+// // const judul = document.getElementById('judul');
+// // judul.innerHTML="<em>Dom manipulasi</em>"
+
+// const judul=document.querySelector('#judul')
+// judul.style.backgroundColor='red';
